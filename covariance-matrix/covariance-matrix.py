77c45b27ed18d15e9f52ab8843691d37fdc77a96
Marwane Ait mouloud <135098625+MarwaneAit-mouloud@users.noreply.github.com>
2026-03-17 15:00:58 +0100

    Update solution for covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -3,23 +3,21 @@ import numpy as np
 def covariance_matrix(X):
     """
     Compute the sample covariance matrix from dataset X.
-    Returns None if the input is not 2-dimensional or has fewer than 2 samples.
     """
     X = np.asarray(X)
     
-    # Strictly enforce that the input must be a 2D matrix (fixes the test)
     if X.ndim != 2:
         return None
         
     N, D = X.shape
     
-    # We need at least 2 samples to compute sample covariance
+
     if N < 2: 
         return None
         
     mu = np.mean(X, axis=0)
     X_centered = X - mu
     
-    # Compute the covariance matrix
+
     return (X_centered.T @ X_centered) / (N - 1)
 
